06021d02c0e7a9ca8a212f7470e8748a169515f3
cmy1230 <20300750023@fudan.edu.cn>
2023-04-14 21:13:08 +0800

    Added code for creating and interconnecting sub_module ports

--- BreakLoop/base_ADG/include/element.h
+++ BreakLoop/base_ADG/include/element.h
@@ -7,6 +7,10 @@ using namespace std;
 #define gib_num 8
 #define track_type 4
 #define element_max 50
+#define max 50000 //栈的大小
+#define spacer "/"//层级分隔符
+#define space '/'
+
 
 const string input_name[] = {"itrackW","itrackN","itrackE","itrackS","opinNE","opinNW","opinSW","opinSE"};
 const string output_name[] = { "otrackW","otrackN","otrackE","otrackS","ipinNE","ipinNW","ipinSW","ipinSE" };
@@ -123,6 +127,377 @@ protected:
 	int* output;//各输出端口类型
 	int* output_id;//各输出端口id
 };
+
+class port {
+public:
+	port() {
+		sub_module = "";
+		sub_id = -1;
+		element = "";
+		element_id = -1;
+		type = "";
+		id = -1;
+		next = NULL;
+		n_next = 0;
+		ahead = NULL;
+		n_ahead = 0;
+		visited = -1;
+	}
+
+	port(string module_name, int module_id, string ele_name, int ele_id, string port_type, int port_id) {
+		sub_module = module_name;
+		sub_id = module_id;
+		element = ele_name;
+		element_id = ele_id;
+		type = port_type;
+		id = port_id;
+	}
+
+	bool print() {
+		cout << "Module:" << sub_module << sub_id << "\tElement:" << element << element_id << "\tType:" << type << "\tId:" << id << "\tNext:" << n_next << endl;
+		return true;
+	}
+
+	bool changesub_module(const string& s) {
+		sub_module = s;
+		return true;
+	}
+
+	bool changesub_id(int num) {
+		sub_id = num;
+		return true;
+	}
+
+	bool changeelement(const string& s) {
+		element = s;
+		return true;
+	}
+
+	bool changeelement_id(int num) {
+		element_id = num;
+		return true;
+	}
+
+	bool changetype(const string& s) {
+		type = s;
+		return true;
+	}
+
+	bool changeid(int num) {
+		id = num;
+		return true;
+	}
+
+	bool assigned(port* interface) {
+		n_ahead++;
+		port** temp = ahead;
+		ahead = new port * [n_ahead];
+		if (temp != NULL) {
+			for (int i = 0; i < n_ahead - 1; i++)
+				ahead[i] = temp[i];
+			delete[] temp;
+		}
+		ahead[n_ahead - 1] = interface;
+		return true;
+	}
+	//连接前置端口
+
+	bool unassigned(port* interface) {
+		port* temp = ahead[n_ahead - 1];
+		for (int i = 0; i < n_ahead; i++)
+			if (ahead[i] == interface) {
+				ahead[--n_ahead] = NULL;
+				ahead[i] = temp;
+				return true;
+			}
+		return false;
+	}
+	//断开前置端口
+
+	bool assign(port* interface) {
+		n_next++;
+		port** temp = next;
+		next = new port * [n_next];
+		if (temp != NULL) {
+			for (int i = 0; i < n_next - 1; i++)
+				next[i] = temp[i];
+			delete[] temp;
+		}
+		next[n_next - 1] = interface;
+		return interface->assigned(this);
+	}
+	//连接后续端口
+
+	bool unassign(port* interface) {
+		port* temp = next[n_next - 1];
+		for (int i = 0; i < n_next; i++)
+			if (next[i] == interface) {
+				next[--n_next] = NULL;
+				next[i] = temp;
+				return interface->unassigned(this);
+			}
+		return false;
+	}
+	//断开后续端口
+
+	bool changevisited(int num) {
+		visited = num;
+		return true;
+	}
+
+	bool getsub_module(string& s) {
+		s = sub_module;
+		return true;
+	}
+
+	int  getsub_id() {
+		return true;
+	}
+
+	bool getelement(string& s) {
+		s = element;
+		return true;
+	}
+
+	int getelement_id() {
+		return element_id;
+	}
+
+	bool gettype(string& s) {
+		s = type;
+		return true;
+	}
+
+	int getid() {
+		return id;
+	}
+
+	port** getnext() {
+		return next;
+	}
+
+	int getnum() {
+		return n_next;
+	}
+
+	port** getahead() {
+		return ahead;
+	}
+
+	int getahead_num() {
+		return n_ahead;
+	}
+
+	int getvisited() {
+		return visited;
+	}
+
+protected:
+	string sub_module;
+	int sub_id;
+	string element;
+	int element_id;
+	string type;
+	int id;
+	port** next;
+	int n_next;
+	port** ahead;
+	int n_ahead;
+	int visited;
+};
+
+class library {
+public:
+	library() {
+		num = 0;
+		ports = NULL;
+	}
+
+	/*~library() {
+		if (ports != NULL && num > 0) {
+			delete[]ports;
+		}
+	}*/
+
+
+	bool clear() {
+		num = 0;
+		delete[]ports;
+		ports = NULL;
+		return true;
+	}
+
+	bool repeat(string& s) {
+		string name = "";
+		for (int i = 0; i < num; i++) {
+			ports[i]->getelement(name);
+			if (name == s)
+				return true;
+		}
+		return false;
+	}
+
+	bool print() {
+		if (ports == NULL)
+			return false;
+		for (int i = 0; i < num; i++)
+			ports[i]->print();
+		return true;
+	}
+
+	int getnum() {
+		return num;
+	}
+
+	port* getport(int n) {
+		if (n >= num)
+			return NULL;
+		return ports[n];
+	}
+
+	/*bool deleteport(int n) {
+		if (n >= num)
+			return false;
+		num--;
+		ports[n] = ports[num];
+		/*port* temp = ports;
+		ports = new port[num];
+		int j = 0;
+		if (temp != NULL)
+			for (int i = 0; i < num + 1; i++)
+				if (i == n)
+					continue;
+				else
+					ports[j++] = temp[i];
+		delete[] temp;
+		return true;
+	}*/
+
+	bool wirte(port* interface) {
+		num++;
+		port** temp = ports;
+		ports = new port * [num];
+		if (temp != NULL)
+			for (int i = 0; i < num - 1; i++)
+				ports[i] = temp[i];
+		ports[num - 1] = interface;
+		if (temp != NULL)
+			delete[] temp;
+		return true;
+	}
+private:
+	int num;
+	port** ports;
+};
+//元件库定义
+
+class stack {
+public:
+	stack() {
+		msize = max;
+		top = -1;
+		i = new port * [max];
+	}
+
+	void clear() {
+		msize = max;
+		top = -1;
+		return;
+	}
+	//清空栈
+
+	bool empty() {
+		if (top <= -1)
+			return true;
+		return false;
+	}
+	//判断栈是否空
+
+	bool full() {
+		if (top >= msize - 1)
+			return true;
+		return false;
+	}
+	//判断栈是否满
+
+	bool push(port* m) {
+		if (top >= msize - 1) {
+			printf("Error in push:stack overflow!\n");
+			return false;
+		}
+		i[++top] = m;
+		return true;
+	}
+	//入栈
+
+	port* pop() {
+		if (top <= -1) {
+			printf("Error in pop:stack is empty!\n");
+			return NULL;
+		}
+		return i[top--];
+	}
+	//出栈
+
+	port* gettop() {
+		if (top <= -1) {
+			printf("Error in pop:stack is empty!\n");
+			return NULL;
+		}
+		return i[top];
+	}
+	//获取栈顶元素
+
+private:
+	int msize;
+	int top;
+	port** i;
+};
+//栈
+
+int* buildnext(string P) {
+	int m = P.length();
+	int j = 0;
+	int* N = new int[m];
+	int t = N[0] = -1;
+	while (j < m - 1)
+		if (t < 0 || P[j] == P[t]) {
+			j++;
+			t++;
+			N[j] = t;
+		}
+		else
+			t = N[t];
+	return N;
+}
+
+int match(string P, string T) {//子串P 原串T
+	int* next = buildnext(P);
+	int n = T.length();
+	int m = P.length();
+	int i = 0, j = 0;
+	while (j < m && i < n)
+		if (0 > j || T[i] == P[j]) {
+			i++;
+			j++;
+		}
+		else
+			j = next[j];
+	delete[] next;
+	if (j >= m)
+		return i - j;
+	else
+		return -1;
+}
+//KMP算法
+
+bool stringtoint(string input, int& output) {
+	output = 0;
+	for (int i = 0; i < input.length(); i++)
+		output = output * 10 + input.at(i) - '0';
+	return true;
+}
+//将只含数字的字符串转化成对应的整型
 
 lut* luts[element_max];
 int lut_num = 0;
--- BreakLoop/base_ADG/src/breakloop_adg.cpp
+++ BreakLoop/base_ADG/src/breakloop_adg.cpp
@@ -1,10 +1,153 @@
 #include<iostream>
 #include<string>
 #include<fstream>
-#include"breakloop.h"
 #include"element.h"
 
 using namespace std;
+
+bool prune(port* interface) {
+	if (interface->getnum() > 0 || interface->getahead_num() == 0)
+		return false;
+	port** ports = interface->getahead();
+	for (int i = 0; i < interface->getahead_num(); i++) {
+		ports[i]->unassign(interface);
+		prune(ports[i]);
+	}
+	return true;
+}
+//对后置端口数为0的端口进行剪枝
+
+int getport_num(const string& buf, int& pointer) {
+	int num = 0;
+	for (; pointer < buf.length() && buf.at(pointer) != ','; pointer++)
+		if (buf.at(pointer) >= '0' && buf.at(pointer) <= '9')
+			num = num * 10 + buf.at(pointer) - '0';
+	return num;
+}
+//从语句中的指定位置开始提取所需整数
+
+string getport_name(const string& buf, int& pointer) {
+	string name = "";
+	for (; pointer < buf.length() && buf.at(pointer) != '\"'; pointer++);
+	for (++pointer; pointer < buf.length() && buf.at(pointer) != '\"'; pointer++)
+		name += buf.at(pointer);
+	return name;
+}
+//从语句中的指定位置开始提取字符串
+
+bool is_equivalence(port* port1, port* port2) {
+	if (port1->getsub_id() != port2->getsub_id())
+		return false;
+	if (port1->getelement_id() != port2->getelement_id())
+		return false;
+	return true;
+}
+//判断是否为同一器件的端口
+
+bool connect(library* list) {
+	int num = list->getnum();
+	string type1, type2, element;
+	port* interface;
+	port* temp;
+	for (int i = 0; i < num; i++) {
+		interface = list->getport(i);
+		interface->getelement(element);
+		if (element == "This")
+			continue;
+		interface->gettype(type1);
+		if (type1 == "I")
+			continue;
+		for (int j = 0; j < num; j++) {
+			if (j == i)
+				continue;
+			temp = list->getport(j);
+			temp->gettype(type2);
+			if (type2 == "O")
+				continue;
+			if (is_equivalence(interface, temp))
+				temp->assign(interface);
+		}
+	}
+	return true;
+}
+//对次级模块中的端口进行互联
+
+bool getsub_library(const string& name, int num, int id, library* list) {
+	ifstream file;
+	int sub_id = 0;
+	string buf;
+	int pointer;
+	int ahead_id, ahead_port, next_id, next_port;
+	string ahead_name, ahead_type, next_name, next_type, type;
+	port* ahead;
+	port* next;
+	port* temp;
+	for (; sub_id < element_num; sub_id++)
+		if (element_id[sub_id] == num)
+			break;
+	if (sub_id == element_num)
+		return false;
+	file.open(name + "_" + to_string(sub_id), ios::in);
+	if (!file.is_open()) {
+		cerr << "Error in getsub_library:file open failed!" << endl;
+		return false;
+	}
+	while (getline(file, buf))
+		if (match("\"connections\"", buf) >= 0)
+			break;
+	while (getline(file, buf)) {
+		if (match("},", buf) >= 0)
+			break;
+		if (match("\"RF\"", buf) > 0 || match("\"Const\"", buf) > 0)
+			continue;//过滤时序器件
+		for (pointer = 0; pointer < buf.length() && buf.at(pointer) != '['; pointer++);
+		ahead_id = getport_num(buf, pointer);
+		ahead_name = getport_name(buf, pointer);
+		pointer += 2;
+		ahead_port = getport_num(buf, pointer);
+		next_id = getport_num(buf, ++pointer);
+		next_name = getport_name(buf, pointer);
+		pointer += 2;
+		next_port = getport_num(buf, pointer);
+		//获得互联语句中的相关信息
+		if (ahead_name == "This")
+			ahead_type = next_type = "I";
+		else if (next_name == "This")
+			ahead_type = next_type = "O";
+		else {
+			ahead_type = "O";
+			next_type = "I";
+		}
+		/*根据端口所属模块名称及顺序判断端口类型，判据如下
+		* 1、当前顶层模块的输入连接次级模块输入
+		* 2、次级模块输出连接当前顶层模块输出
+		* 3、次级模块输出连接次级模块输入
+		*/
+		ahead = NULL;
+		next = NULL;
+		for (int i = 0; i < list->getnum(); i++) {
+			temp = list->getport(i);
+			temp->gettype(type);
+			if (temp->getelement_id() == ahead_id && temp->getid() == ahead_port && type == ahead_type)
+				ahead = temp;
+			if (temp->getelement_id() == next_id && temp->getid() == next_port && type == next_type)
+				next = temp;
+		}
+		if (ahead == NULL) {
+			ahead = new port(name, id, ahead_name, ahead_id, ahead_type, ahead_port);
+			list->wirte(ahead);
+		}
+		if (next == NULL) {
+			next = new port(name, id, next_name, next_id, next_type, next_port);
+			list->wirte(next);
+		}
+		ahead->assign(next);
+	}
+	connect(list);
+	file.close();
+	return true;
+}
+//根据次级模块的adg表述构建对应端口并形成互联关系
 
 int getnumber(const string& buf) {
 	int start;
